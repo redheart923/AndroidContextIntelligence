ec302a4155c14e3bbb3e4f05b0b50f526c8d86cd
azimao <azimao@dev.com>
2026-07-20 10:55:50 +0800

    chore: untrack android-context-current to treat installers as single source of truth

--- android-context-current/queries/summary.sql
+++
@@ -1,9 +0,0 @@
-SELECT node_type, COUNT(*) AS count
-FROM node
-GROUP BY node_type
-ORDER BY count DESC;
-
-SELECT edge_type, COUNT(*) AS count
-FROM edge
-GROUP BY edge_type
-ORDER BY count DESC;
--- android-context-current/queries/workspace_coverage_summary.sql
+++
@@ -1,6 +0,0 @@
-SELECT json_extract(properties_json, '$.repository') AS repository,
-       node_type, COUNT(*) AS count
-FROM node
-WHERE json_extract(properties_json, '$.repository') IS NOT NULL
-GROUP BY repository, node_type
-ORDER BY repository, count DESC;
